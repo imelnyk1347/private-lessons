a28c107c6f0af1be66ef81dc7eb3c339d5d2009d
Ihor Melnyk <ihor.m@corefy.com>
2023-07-02 23:20:01 +0300

    add second homework

--- for_tests.py
+++ for_tests.py
@@ -26,17 +26,61 @@
 # ----------------------------------------------------------------------------------------------
                 # 0  1  2  3        4           5
                 #-6 -5 -4  -3         -2         -1
-name_for_list = [0, 1, 2, 'name', 'some name', 10.50]
-name_for_tuple = (15, 87, 'Artem', 'school', 'Ukraine', 10.2564)
-name_for_string = 'Artem'
+# name_for_list = [0, 1, 2, 'name', 'some name', 10.50]
+# name_for_tuple = (15, 87, 'Artem', 'school', 'Ukraine', 10.2564)
+# name_for_string = 'Artem'
 
 # print(name_for_list[1:4])
 # print(name_for_tuple[4:6])
 # print(name_for_string[2:4])
 
-print(name_for_list[-5:-2])  # від -2 до кінця
-print(name_for_tuple[-5:-2])
-print(name_for_string[-3:])
+
+# Дати домашку по зрізам)))
+
+# print(name_for_list[-5:-2])  # від -2 до кінця
+# print(name_for_tuple[-5:-2])
+# print(name_for_string[-3:])
+
+# ----------------------------------------------------------------------------------------------
+
+# bool examples
+# name = 'Artem'
+#
+# name_one = 'Vasyl'
+#
+# print(name is name)
+# print(name_one is name_one)
+# print(name is name_one)
+# print(name_one is name)
+
+# ----------------------------------------------------------------------------------------------
+# 'Конкатенація' тобто додавання стрічок або додавання двох перемінних
+# name = '50'
+# name_1 = '50'
+# print(name + name_1)
 
 
-# нагадати за булеві типи даних
+# дублювання
+
+# name = 'Artem'
+# print(name * 5)
+
+# len
+
+# name = 'Artem'
+# print(len(name))
+
+
+# index
+#
+# "name = 'Artem'
+
+# print(name[0:2])
+# print(name[:3])"
+
+
+name = 'this is on lower case'
+print(name.isupper())
+print(name.islower())
+print(name.split())
+print(name.title())
--- homework_1.py
+++ homework_1.py
@@ -5,6 +5,11 @@
    тип даних буде не відомий, то має бути виведено таке повідомлення "TypeError: Unknown type data"
 
 """
+# obj = 25
+# if type(obj) in (str, int, float, tuple, list, dict):
+#     print(f"Типом даних перемінної 'obj' є {type(obj)}")
+# else:
+#     print("TypeError: Unknown type data")
 
 """
 
@@ -32,6 +37,29 @@
 
 """
 
+# name = input('Please, enter your name: ')
+#
+# list_of_names = [
+#     'Артем', 'Денис', 'Данііл', 'Данило', 'Олександр', 'Андрій', 'Богдан', 'Дмитро',
+#     'Нікіта', 'Микита', 'Назар', 'Кіріл', 'Кирило', 'Віктор', 'Віталій', 'Гліб',
+#     'Єгор', 'Іван', 'Ілля', 'Артур', 'Вадим', 'Тимофій', 'Михайло', 'Павло',
+#     'Микола', 'Євген', 'Сергій', 'Арсен', 'Юрій', 'Тимур', 'Давид', 'Марк',
+#     'Анна', 'Софія', 'Мілана', 'Злата', 'Дарина', 'Соломія', 'Вікторія',
+#     'Анастасія', 'Дарія', 'Єва', 'Марія', 'Маргарита', 'Аделіна', 'Аліна', 'Поліна',
+#     'Вероніка', 'Орися', 'Меланія', 'Діана', 'Яна', 'Анна', 'Любов', 'Катерина',
+#     'Уляна', 'Олександра', 'Варвара', 'Sophia', 'Emily', 'Mary', 'Linda', 'Madison', 'Susan',
+#     'Lily', 'Michelle', 'Margaret', 'Tracy', 'Alice', 'Sandy', 'Rose', 'Grace', 'Allison',
+#     'Lucy', 'Mila', 'Cindy', 'Naomi', 'Jane', 'Hannah', 'Julie', 'Janet', 'Melanie', 'Stacy',
+#     'Sarah', 'Jake', 'Jack', 'Harry', 'Jacob', 'George', 'James', 'William', 'Connor', 'Joe',
+#     'Liam', 'Ethan', 'Richard', 'Michael', 'Justin', 'Parker', 'Matt', 'Luke', 'Neil',
+#     'Martin', 'Paul', 'John', 'Peter'
+# ]
+#
+# if name in list_of_names:
+#     print('Correct')
+# else:
+#     print('No')
+
 """
 
 3. програма має дві перемінні "first_number" і "second_number" ( перемінні динамічні - підказка, використати input() ): 
@@ -46,6 +74,25 @@
    "Сума двох значень більша за 201 і становить <тут потрібно буде виводити суму>"
    
 """
+
+# first_number = int(input('First: '))
+# second_number = int(input('Second: '))
+#
+# third_number = first_number + second_number
+#
+# if 50 < third_number < 80:
+#     print(f"Сума двох значень {third_number} знаходиться між 50 і 80")
+# elif 120 > third_number > 110:
+#     print(f"Сума двох значень {third_number} менша за 120, але більша за 110")
+# elif 100 < third_number < 109:
+#     print(f"Сума двох значень {third_number} більша за 100, але менша 109")
+# elif 149 < third_number < 200:
+#     print(f"Сума двох значень {third_number} більша за 149, але менша за 200")
+# elif third_number > 201:
+#     print(f"Сума двох значень більша за 201 і становить {third_number}")
+# else:
+#     print('ValueError')
+
 
 """
 
@@ -104,3 +151,62 @@
 }
 
 """
+
+countries = {
+    1: 'Ukraine',
+    2: 'USA',
+    3: 'Canada',
+    4: 'United Kingdom',
+    5: 'Germany',
+    6: 'France',
+    7: 'Italy',
+    8: 'Spain',
+    9: 'Australia',
+    10: 'Japan',
+    11: 'China',
+    12: 'Russia',
+    13: 'Brazil',
+    14: 'Mexico',
+    15: 'Argentina',
+    16: 'India',
+    17: 'South Korea',
+    18: 'Netherlands',
+    19: 'Switzerland',
+    20: 'Sweden',
+    21: 'Belgium',
+    22: 'Norway',
+    23: 'Denmark',
+    24: 'Finland',
+    25: 'Poland',
+    26: 'Austria',
+    27: 'Greece',
+    28: 'Portugal',
+    29: 'Ireland',
+    30: 'New Zealand',
+    31: 'Czech Republic',
+    32: 'Turkey',
+    33: 'Hungary',
+    34: 'Thailand',
+    35: 'Israel',
+    36: 'Egypt',
+    37: 'South Africa',
+    38: 'Saudi Arabia',
+    39: 'United Arab Emirates',
+    40: 'Singapore',
+    41: 'Malaysia',
+    42: 'Indonesia',
+    43: 'Vietnam',
+    44: 'Philippines',
+    45: 'Colombia'
+}
+
+number = int(input('Enter a number from 1 to 45: '))
+
+for key, value in countries.items():
+    if number > 45:
+        print("Your choice is not correct !")
+        break
+    elif number != key:
+        continue
+    elif number == key:
+        print(value)
