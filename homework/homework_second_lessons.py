035b863861fe4772f2fbbac8a7d99b8e01d44b14
Ihor Melnyk <ihor.m@corefy.com>
2023-07-06 20:35:50 +0300

    update .gitignore file,refactoring project

--- homework/homework_second_lessons.py
+++ homework/homework_second_lessons.py
@@ -5,14 +5,21 @@
 
 """
 
-
-
 """
 2. Написати скріпт, який шукатиме в значенні стріки "This is amazing string" окремі слова,
 якщо слово не буде знайдено, то в консолі програма виведе повідомлення 'ValueError:parameter not found'
 
 """
-
+# word = input("Please inter yur word: ")
+#
+# default_value = "This is amazing string"
+#
+# n = default_value.find(word)
+#
+# if n == -1:
+#     print('ValueError:parameter not found')
+# else:
+#     print(f'Word "{word}" find on default value')
 
 """
 3. Написати скріпт, який видалить із стрічки "    This is amazing string." усі пробіли.
@@ -21,6 +28,11 @@
 в виведе "This is amazing string."
 """
 
+# input_some_word = input("Please, input some word: ")
+# print(input_some_word.strip(' '))
+
+# name = input('Enter: some string: '.strip())
+# print(name)
 
 
 """
@@ -28,19 +40,31 @@
 Наприклад, my_awsome_string = 'My name is Artem', а виведе 'My name is Volodymyr'
 
 """
+# my_awsome_string = 'My name is Artem'
+# print(my_awsome_string.replace('Artem', 'Volodymyr'))
 
 
 """
-5.Написати скріпт, який буде ставити пробіл зліва і справа кожної букви.
+5.1. Написати скріпт, який буде ставити пробіл зліва і справа кожної букви.
 Наприклад, some_string = 'Wolfeschlegelsteinhausenbergerdorff', 
 а буде виведено 'W o l f e s c h l e g e l s t e i n h a u s e n b e r g e r d o r f f'
 
-5.1 Написати скріпт , який об'єднає всі букви в одне слово.
+5.2 Написати скріпт , який об'єднає всі букви в одне слово.
 Наприклад, some_string = 'Wolfeschlegelste', second_string = 'inhausenbergerdorff'
 а виведе 'Wolfeschlegelsteinhausenbergerdorff'
 """
+# some_string = input('Enter some word`s: ')
+#
+# print(' '.join(some_string))
 
 
+# some_string = 'Wolfeschlegelste'
+# second_string = 'inhausenbergerdorff'
+
+# some_string = input('Enter your first word: ')
+# second_string = input('Enter your second word: ')
+#
+# print(''.join([some_string, second_string]))
 
 """
 6. Програма приймає будь-яке слово / або слова і виводить в консоль перші літери з великої букви.
@@ -52,3 +76,14 @@
 6.2. Програма приймає будь-яке слово / або слова і виводить в консоль всі літери з маленької букви. 
 Наприклад, приймає слово 'WoRd', 'word'. 
 """
+# 6.1
+# first_example = input('Enter some text: ')  # or .title()
+# print(first_example.title())  # or just print(first_example)
+
+# 6.2
+# first_example = input('Enter some text: ')  # or .upper()
+# print(first_example.upper())  # or just print(first_example)
+
+# 6.3
+# first_example = input('Enter some text: ')  # or .lower()
+# print(first_example.lower())  # or just print(first_example)
