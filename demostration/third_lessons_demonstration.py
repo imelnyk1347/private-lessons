1f59aeb4fb7c5ff5ffcf5ae324135e07c9045fbd
Ihor Melnyk <ihor.m@corefy.com>
2023-07-06 21:19:01 +0300

    add lessons theme for third lessons

---
+++ demostration/third_lessons_demonstration.py
@@ -0,0 +1,20 @@
+# Методи str
+
+# isspace() - https://www.w3schools.com/python/ref_string_isspace.asp
+# isalpha() - https://www.w3schools.com/python/ref_string_isalpha.asp
+# islower() - https://www.w3schools.com/python/ref_string_islower.asp
+# isupper() - https://www.w3schools.com/python/ref_string_isupper.asp
+# isdigit() - https://www.w3schools.com/python/ref_string_isdigit.asp
+# isalnum() - https://www.w3schools.com/python/ref_string_isalnum.asp
+# isdecimal() - https://www.w3schools.com/python/ref_string_isdecimal.asp
+
+# index() - https://www.w3schools.com/python/ref_string_index.asp
+# istitle() - https://www.w3schools.com/python/ref_string_istitle.asp
+# ljust() - https://www.w3schools.com/python/ref_string_ljust.asp
+# format() - https://www.w3schools.com/python/ref_string_format.asp
+# find() - https://www.w3schools.com/python/ref_string_find.asp
+# endswith() - https://www.w3schools.com/python/ref_string_endswith.asp
+# count() - https://www.w3schools.com/python/ref_string_count.asp
+# center() - https://www.w3schools.com/python/ref_string_center.asp
+# casefold() - lower() - https://www.w3schools.com/python/ref_string_casefold.asp
+# capitalize() - https://www.w3schools.com/python/ref_string_capitalize.asp
