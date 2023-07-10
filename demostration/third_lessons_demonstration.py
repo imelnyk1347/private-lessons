ec596c6ae2642a9e7d93f9314419935047476dfd
Ihor Melnyk <ihor.m@corefy.com>
2023-07-10 23:06:46 +0300

    add theoretical and practical third lessons/homework

--- demostration/third_lessons_demonstration.py
+++ demostration/third_lessons_demonstration.py
@@ -7,14 +7,112 @@
 # isdigit() - https://www.w3schools.com/python/ref_string_isdigit.asp
 # isalnum() - https://www.w3schools.com/python/ref_string_isalnum.asp
 # isdecimal() - https://www.w3schools.com/python/ref_string_isdecimal.asp
+# istitle() - https://www.w3schools.com/python/ref_string_istitle.asp
 
 # index() - https://www.w3schools.com/python/ref_string_index.asp
-# istitle() - https://www.w3schools.com/python/ref_string_istitle.asp
 # ljust() - https://www.w3schools.com/python/ref_string_ljust.asp
 # format() - https://www.w3schools.com/python/ref_string_format.asp
 # find() - https://www.w3schools.com/python/ref_string_find.asp
 # endswith() - https://www.w3schools.com/python/ref_string_endswith.asp
-# count() - https://www.w3schools.com/python/ref_string_count.asp
+# count() - https://www.w3schools.com/python/ref_string_count.asp       !!!
 # center() - https://www.w3schools.com/python/ref_string_center.asp
 # casefold() - lower() - https://www.w3schools.com/python/ref_string_casefold.asp
 # capitalize() - https://www.w3schools.com/python/ref_string_capitalize.asp
+
+
+# 1. istitle(), title(), isspace(), upper()
+# 2. isalpha(), isupper(), upper(), isalnum(), isdigit()
+# 3. count(),isspace(), format()
+# 4. format()
+# --------------------------------------------------------------------------------------------------------------------
+
+# name = input('Enter your name: ')
+#
+# if name.isspace():
+#     print('Please, enter your name')
+# else:
+#     print(f'Your name is {name}')
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# name = input('Enter your name: ')
+#
+# result = name.isalpha()
+#
+# if not result:
+#     print('Please, enter your name')
+# else:
+#     print(f'Your name is {name}')
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# name = input('Enter your name: ')
+#
+# result = name.islower()
+#
+# if not result:
+#     print('Your name in upper case !')
+# else:
+#     print(f'Your name is {name}')
+# --------------------------------------------------------------------------------------------------------------------
+
+# name = input('Enter your name: ')
+#
+# result = name.isdigit()
+#
+# if result:
+#     print('AttributeERROR: THIS IS NOT WORD')
+# else:
+#     print(f'Your name is {name}')
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# city = input('Enter your city: ')
+#
+# result = 'Hello,{}. Your age is {}. You live in {}'.format(name, age, city)
+#
+# print(result)
+#
+#
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# city = input('Enter your city: ')
+#
+# result = 'Hello,{0}. Your age is {1}. You live in {2}'.format(name, age, city)
+#
+# print(result)
+
+
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# city = input('Enter your city: ')
+#
+# result = f'Hello,{name}. Your age is {age}. You live in {city}'
+#
+# print(result)
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# txt = "Hello, And Welcome To My WORLD!"
+#
+# x = txt.casefold()
+# y = txt.lower()
+#
+# print(x)
+#
+# print()
+#
+# print(y)
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# txt = "artem, welcome to my world."
+#
+# x = txt.capitalize()
+#
+# print(x)
---
+++ homework/homework_third_lessons.py
@@ -0,0 +1,122 @@
+"""
+1. Написати скрипт, який прийматиме речення.
+   Наприклад, юзер вводить речення: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
+   а скрипт робить перевірку:
+    - якщо всі слова розпочинаються з великої букви, то перевести всі слова в нижній регістр;
+    - якщо слова розпочинаються з маленької букви, то перевести у верхній регістр;
+    - якщо юзер взагалі нічого не введе, то буде виведено "InputError:nothing has been entered"
+
+"""
+from pprint import pprint
+
+# user_input = input("Please, enter the text: ")
+#
+# if len(user_input) == 0:
+#     print('InputError:nothing has been entered!')
+# elif not user_input.istitle():
+#     print(user_input.title())
+# elif user_input.istitle():
+#     print(user_input.lower())
+# else:
+#     print('Error: unexpected result!')
+"""
+2. Написати скрипт, який прийматиме на перевірку слово.
+   Наприклад, юзер вводить 'SpaceX' і скрипт:
+    - якщо всі символи в слові складаються із букв, то буде виведено "All characters are letters";
+    - якщо всі символи  з великої букви, то повідомлення "All characters are letters" буде виведено повністю
+      у верхньому регістрі;
+    - якщо в слові будуть і символи літер і цифр, то буде виведено повідомлення 'A mixed data type was entered';
+    - якщо будуть введені лише символи цифр, то буде виведено повідомлення 'Only numeric characters were entered';
+    - якщо не буде нічого введено, то буде виведено "InputError:nothing has been entered"
+
+"""
+# word = input('Enter a word: ')
+# if len(word) == 0:
+#     print('InputError:nothing has been entered')
+# elif word.isdigit():
+#     print('Only numeric characters were entered')
+# elif word.isupper():
+#     print('All characters are letters'.upper())
+# elif word.isalpha():
+#     print('All characters are letters')
+# elif word.isalnum():
+#     print('A mixed data type was entered')
+# else:
+#     print('Error: unexpected result!')
+"""
+3. Написати скрипт, який виведе речення для юзера (приклад речення буде нижче) і дасть можливість юзеру ввести слово. 
+    - якщо слово буде знайдено у тексті, то вивести і саме слово, і кількість його повторень;
+    - якщо слово не буде знайдено у тексті, то вивести 'The search for the word "" was not found.', де
+      у "" - вивести що саме було введено юзером;
+    - якщо були введені самі пробіли, то вивести 'Incorrect search data';
+    - якщо не буде нічого введено, то буде виведено "InputError:nothing has been entered".
+    
+    Текст , який буде виведено для юзера:
+    
+    'Україна має декілька історичних назв, що є частково або повністю тотожними.Україна має декілька історичних назв, що є частково або повністю тотожними'
+
+"""
+# text = 'Україна має декілька історичних назв, що є частково або повністю тотожними.Україна має декілька історичних ' \
+#        'назв, що є частково або повністю тотожними'
+# print(text)
+#
+# user_input = input('Enter word to search: ')
+#
+# if user_input.isspace():
+#     print('Incorrect search data')
+# elif text.count(user_input) == 0:
+#     if user_input.isdigit():
+#         print(f'The search for the digit "{user_input}" was not found.')
+#         exit()
+#     print(f'The search for "{user_input}" was not found.')
+# elif text.count(user_input) > 0:
+#     print(f'Find: "{user_input}". Count: {text.count(user_input)}')
+
+"""
+4. Програма прийматиме дані юзера на вхід: ім'я, прізвище, вік, місце проживання, кількість батьків і виводитиме 
+   введену інформацію в такому вигляді: 'Ваші дані успішно прийнято. Ваше прізвище та ім'я ""., Ваше місце народження та
+   фактичне проживання "", Ви маєте "" батьків.', де в "" - потрібно, щоб були підставлені відповідні дані введені 
+   користувачем.
+    
+   - якщо ім'я користувача було введено лише літерами і з маленької букви, то скрипт має виводити ім'я з великої літери;
+   - якщо прізвище користувача було введено лише літерами і з маленької букви, то скрипт має виводити прізвище 
+     з великої літери;
+   - якщо місце проживання користувача було введено лише літерами і з маленької букви, то скрипт має виводити 
+     місце проживання з великої літери;
+   - якщо вік було введено буквами, а не цифрою, то після основного повідомлення в кінці буде виведено таке речення:
+     'Ваш вік "" та кількість батьків "" було введено літерами, будь ласка, в майбутньому введіть цифри' - 
+      і в "" потрібно буде підставити що саме ввів буквами юзер;
+   - якщо в ім'я, прізвище та місце проживання буде введено будь-що окрім літер - вивести повідомлення 
+     'A mixed data type was entered';
+   - якщо вік буде більший за 100 років та кількість батьків буде більше 2, то вивести 
+     "InputError: incorrect input data";
+   - якщо жодне поле не буде введено, то вивести "InputError:nothing has been entered"  
+
+"""
+# user_name = input("Enter user name: ")
+# user_surname = input("Enter user surname: ")
+# user_living = input("Enter user living: ")
+# user_age = input("Enter user age: ")
+# user_parents = input("Enter user parents: ")
+#
+#
+# if len(user_name) == 0 and len(user_surname) == 0 and len(user_living) == 0 and len(user_age) == 0 \
+#       and len(user_parents) == 0:
+#     print('InputError:nothing has been entered')
+#     exit()
+# if user_name.isalpha() and user_surname.isalpha() and user_living.isalpha():
+#     if user_name.islower() and user_surname.islower() and user_living.islower():
+#         print(user_name.title(), user_surname.title(), user_living.title())
+#         if not user_age.isdigit() and not user_parents.isdigit():
+#             print(f'Ваші дані успішно прийнято. Ваше прізвище та ім\'я {user_name} {user_surname}, '
+#                   f'Ваше місце народження та фактичне проживання {user_living}, Ви маєте {user_parents} батьків.'
+#                   f'Ваш вік {user_age} та кількість батьків {user_parents} було введено літерами, будь ласка, '
+#                   f'в майбутньому введіть цифри.')
+#         if user_age.isdigit() and user_parents.isdigit():
+#             if user_age > '100' and user_parents > '2':
+#                 print('InputError: incorrect input data')
+#             print(f'Ваші дані успішно прийнято. Ваше прізвище та ім\'я {user_name} {user_surname}, '
+#                   f'Ваше місце народження та фактичне проживання {user_living}, Ви маєте {user_parents} батьків.')
+#     print('error from lower case')
+# else:
+#     print('A mixed data type was entered')
