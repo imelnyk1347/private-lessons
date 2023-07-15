a684e9bee7a98aebcc5ff4a92e1b6adfa23e4863
Ihor Melnyk <ihor.m@corefy.com>
2023-07-15 16:25:12 +0300

    add fourth demonstration lessons

---
+++ demostration/fourth_lessons_demonstration.py
@@ -0,0 +1,55 @@
+# Integers. STRING special characters
+
+# str
+
+# http://ruslan.rv.ua/python-starter/strings/escapes.html
+
+# n = 'This is new line.\nThis is second line'
+# print(n)
+
+#----------------------------------------------------------------
+
+# n = 'This is new line\rThis is second line'
+# print(n)
+
+#----------------------------------------------------------------
+
+# n = 'This is new line.\tThis is second line'
+# print(n)
+
+#----------------------------------------------------------------
+
+# n = 'This is new line.\aThis is second line'
+# print(n)
+
+#----------------------------------------------------------------
+
+# n = 'This is new line.\'This is second line\''
+#
+# t = "This is new line.\"This is second line\""
+#
+# s = "This is new line.\\This is second line\\"
+#
+# print(n, t, s, sep='\n', end='.\n')
+
+#----------------------------------------------------------------
+
+# integers
+
+# http://ruslan.rv.ua/python-starter/vars_and_types/numbers.html#_2
+
+#----------------------------------------------------------------
+# List/Array
+
+# append() -> https://www.w3schools.com/python/ref_list_append.asp
+# clear() -> https://www.w3schools.com/python/ref_list_clear.asp
+# copy() -> https://www.w3schools.com/python/ref_list_copy.asp
+# count() -> https://www.w3schools.com/python/ref_list_count.asp
+# extend() -> https://www.w3schools.com/python/ref_list_extend.asp
+# index() -> https://www.w3schools.com/python/ref_list_index.asp
+# insert() -> https://www.w3schools.com/python/ref_list_insert.asp
+# pop() -> https://www.w3schools.com/python/ref_list_pop.asp
+# remove() -> https://www.w3schools.com/python/ref_list_remove.asp
+# reverse() -> https://www.w3schools.com/python/ref_list_reverse.asp
+
+#----------------------------------------------------------------
