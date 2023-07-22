d96e8f34436aa3be40a345042040150483f562b4
Ihor Melnyk <ihor.m@corefy.com>
2023-07-22 16:44:44 +0300

    add fifth demonstration lessons

---
+++ demostration/fifth_lessons_demonstration.py
@@ -0,0 +1,158 @@
+"""
+Agenda
+
+1. Circles 'for' / 'while'
+2. range() ;  https://www.freecodecamp.org/ukrainian/news/tsykl-for-u-python-pryklad-syntaksysu/
+3. cirle for in list / string;
+4. break , continue in for / while circles ;
+5. pass()
+
+presentation: https://docs.google.com/presentation/d/1qaukUA4OLjYnZgc1ylE_YqDrrvua5vsNlLMj2rmZwY4/edit?usp=sharing
+
+for: https://www.w3schools.com/python/python_for_loops.asp ; https://www.freecodecamp.org/ukrainian/news/tsykl-for-u-python-pryklad-syntaksysu/
+
+while: https://www.w3schools.com/python/python_while_loops.asp
+
+loops inside loops: https://www.w3schools.com/python/gloss_python_for_nested.asp
+
+range: http://ruslan.rv.ua/python-starter/loop_for/range.html
+
+pass: https://www.w3schools.com/python/gloss_python_for_pass.asp
+
+continue: https://www.w3schools.com/python/gloss_python_for_continue.asp
+
+break: https://www.w3schools.com/python/gloss_python_for_break.asp
+
+"""
+example = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+for number in range(1, len(example), 2):
+    print(number)
+
+
+# example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+#
+# for number in example:
+#     if number in (1, 2, 3, 4, 5, 6, 7):
+#         continue
+#     elif number == 8:
+#         print('This is 8')
+#     else:
+#         break
+
+# name = 10
+# for i in range(1, 15, 2):
+#   print(name * i)
+
+
+
+# for i in range(4):
+#     print(i)
+#     print(i ** 2)
+# print('Конец цикла')
+
+# number = True
+# while number:
+#     name = input('Enter your name: ')
+#     second_name = input('Enter second name: ')
+#     if name != second_name:
+#         print('False')
+#     else:
+#         print('True')
+#         # number = False
+#         # break
+#
+# print('End of circle while.')
+
+
+# listeng = [1, 2, 3, 4, 5]
+#
+# i = 0
+#
+# while i < len(listeng):
+#     if listeng[i] % 2 == 0:
+#         print(listeng[i])
+#     i += 1
+#
+# my_list = [1, 2, 3, 4, 5]
+#
+# i = 0
+#
+# while my_list[i] != 5:
+#     print(my_list[i])
+#     i += 1
+#
+# list_of_words = ["apple", "banana", "cherry"]
+#
+# for word in list_of_words:
+#     print(word)
+#
+# list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# for list in list_of_lists:
+#     sum = 0
+#     for number in list:
+#         sum += number
+#     print(sum)
+#
+# list_of_lists = [[1, 2], [3, 4], [5, 6]]
+#
+# for list in list_of_lists:
+#     print(list[0], list[1])
+#
+# list_of_words = ["apple", "banana", "cherry", "orange", "lemon"]
+# fruits = ["apple", "banana", "cherry"]
+# for word in list_of_words:
+#     if word in fruits:
+#         print(word, "is a fruit")
+#     else:
+#         print(word, "is not a fruit")
+
+# list_of_words = ["apple", "banana", "cherry", "orange", "lemon"]
+# fruits = input('Enter fruit, please: ')
+#
+# for word in list_of_words:
+#     if fruits != word:
+#         print('Not found')
+#     else:
+#         print('True')
+#         break
+
+# Ітерація через список списків і виведення суми всіх чисел у кожному списку
+
+# list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for list_of_numbers in list_of_lists:
+#     total = 0
+#     for number in list_of_numbers:
+#         total += number
+#
+# print(total)
+#
+# # Ітерація через список і перевірка, чи є кожне слово у списку "fruits"
+#
+# list_of_words = ["apple", "banana", "cherry", "orange", "lemon"]
+# fruits = ["apple", "banana", "cherry"]
+# for word in list_of_words:
+#     if word in fruits:
+#         print(word, "is a fruit")
+#     else:
+#         print(word, "is not a fruit")
+#
+# # Ітерація через список і додавання кожного слова до нового списку
+#
+# list_of_words = ["apple", "banana", "cherry"]
+# new_list = []
+# for word in list_of_words:
+#     new_list.append(word)
+#     print(new_list)
+
+# list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for list_of_numbers in list_of_lists:
+#     total = 0
+#     for number in list_of_numbers:
+#         total += number
+#         print(total, end=' ', sep='')
+#         print()
+#     print(total, end='\n')
+#     print()
+# print(total, end='\n')
--- demostration/fourth_lessons_demonstration.py
+++ demostration/fourth_lessons_demonstration.py
@@ -4,7 +4,7 @@
 
 # http://ruslan.rv.ua/python-starter/strings/escapes.html
 
-# n = 'This is new line.\nThis is second line'
+# n = 'This is first line.\nThis is second line'
 # print(n)
 
 #----------------------------------------------------------------
@@ -14,12 +14,12 @@
 
 #----------------------------------------------------------------
 
-# n = 'This is new line.\tThis is second line'
+# n = 'This is new line.\t\t\t\t\t\t\t\tThis is second line'
 # print(n)
 
 #----------------------------------------------------------------
 
-# n = 'This is new line.\aThis is second line'
+# n = 'This is new line. This is second line\a'
 # print(n)
 
 #----------------------------------------------------------------
@@ -30,7 +30,9 @@
 #
 # s = "This is new line.\\This is second line\\"
 #
-# print(n, t, s, sep='\n', end='.\n')
+# print(n, t, s)
+#
+# print(n, t, s, sep='\n', end='\n')
 
 #----------------------------------------------------------------
 
@@ -38,8 +40,22 @@
 
 # http://ruslan.rv.ua/python-starter/vars_and_types/numbers.html#_2
 
+# first_string = 11
+# second_string = '1'
+# print(second_string + second_string)
+# print(first_string + first_string)
+# print(first_string % 2)
+# print(first_string ** 2)
+# print(first_string + second_string)
+# print(second_string / 100)
+# print(second_string + 100)
+# print(second_string * 100)
+
 #----------------------------------------------------------------
-# List/Array
+# List/Array - []
+
+# l = []
+# print(type(l))
 
 # append() -> https://www.w3schools.com/python/ref_list_append.asp
 # clear() -> https://www.w3schools.com/python/ref_list_clear.asp
@@ -51,5 +67,53 @@
 # pop() -> https://www.w3schools.com/python/ref_list_pop.asp
 # remove() -> https://www.w3schools.com/python/ref_list_remove.asp
 # reverse() -> https://www.w3schools.com/python/ref_list_reverse.asp
+# sort() - https://www.w3schools.com/python/ref_list_sort.asp
 
 #----------------------------------------------------------------
+
+# різниця між append та insert
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits_1 = ['apple', 'banana', 'cherry']
+#
+# fruits.insert(1, "orange")
+# fruits_1.append("orange")
+#
+# print(fruits)
+# print(fruits_1)
+
+#----------------------------------------------------------------
+# різниця між clear та pop
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits_1 = ['apple', 'banana', 'cherry']
+#
+# fruits.pop(2)
+# fruits_1.clear()
+#
+#
+# print('This is how pop work: ', fruits)
+# print('This is how clear work: ', fruits_1)
+
+#----------------------------------------------------------------
+# різниця між remove та pop
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits_1 = ['apple', 'banana', 'cherry']
+#
+# fruits.remove("cherry")
+# fruits_1.pop(2)
+#
+# print(fruits)
+# print(fruits_1)
+#----------------------------------------------------------------
+
+
+cars = ['Ford', 'BMW', 'Volvo']
+cars_1 = ['Ford', 'BMW', 'Volvo']
+
+cars.sort(reverse=True)
+cars_1.sort()
+
+print(cars)
+print(cars_1)
